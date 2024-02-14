2c31124bd535c6cc2d64c1e2f13b3633c1b236fd
Shreyans Jain <shreyansjain@Shreyanss-MacBook-Air.local>
2024-02-14 10:50:18 +0530

    added feature importance integration

--- src/hmm.py
+++ src/hmm.py
@@ -1,10 +1,12 @@
 import glob
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
 from scipy.stats import zscore
 import numpy as np
-from hmmlearn import hmm
+from hmmlearn import hmm, _hmmc
 from tqdm import trange
+from utils import break_list_by_lengths, find_i_followed_by_j, softmax_with_overflow
 
 
 class HMM():
@@ -80,8 +82,13 @@ class HMM():
 
         return best_score, best_model, aics_buf, bics_buf, scores_buf
 
+    def save_model(self, model_path, model):
+
+        with open(model_path + ".pkl", "wb") as f:
+            pickle.dump(model, f)
+
     def get_avg_log_likelihood(self, data_dir, cols, sort=True,
-                               sort_col="step", first_n=None, test_size=0.2,
+                               sort_col="epoch", first_n=None, test_size=0.2,
                                seed=0):
 
         best_scores = []
@@ -117,3 +124,120 @@ class HMM():
             "bics": bics,
             "best_models": best_models,
         }
+
+    def get_features_for_transition(self, model, data, best_predictions,
+                                    lengths, phase_1, phase_2):
+        '''
+        For each time a transition (phase_1 -> phase_2) happens,
+        compute the derivatives for each feature.
+
+        This computation is slightly inefficient, in that it computes the
+        entire forward lattice of derivatives.
+
+        In practice, this inefficiency doesn't seem to be an issue in
+        terms of runtime.
+        '''
+        features = []
+        for (i, datum) in enumerate(break_list_by_lengths(data, lengths)):
+            preds = best_predictions[i]
+            indexes = find_i_followed_by_j(preds, phase_1, phase_2)
+            if indexes != []:
+                derivatives = np.array(self.get_derivatives(datum, model))
+                for idx in indexes:
+                    features.append(derivatives[idx, phase_2])
+        return features
+
+    def get_difference_bt_means(self, model, phase_1, phase_2):
+        """
+        Returns the difference between mean values of two different phases
+
+        Args:
+            model : Trained HMM Model 
+            phase_1 (int): Source Phase of a transition
+            phase_2 (int): Destination phase of a transition
+
+        """
+        return model.means_[phase_2] - model.means_[phase_1]
+
+    def get_derivatives(self, X, model):
+        '''
+        Compute the derivative d/dz_t p(s_t = k | z_{1:t}) for the entire
+        forward lattice.
+        '''
+        derivatives = []
+
+        log_frameprob = model._compute_log_likelihood(X)
+        log_probij, fwdlattice = _hmmc.forward_log(
+                    model.startprob_, model.transmat_, log_frameprob)
+        n_components = fwdlattice.shape[1]  # can be computed another way
+        covars = [np.linalg.inv(model.covars_[i]) for i in range(n_components)]
+
+        for i in range(len(X)):
+            derivatives_i = []
+            probs = softmax_with_overflow(fwdlattice[i]) 
+            Z = np.sum([probs[j] * covars[j] @ (model.means_[j] - X[i]) for j in range(n_components)])
+
+            for component in range(n_components):
+                derivatives_i.append(
+                    covars[component] @ (model.means_[component] - X[i]) - Z 
+                )
+
+            derivatives.append(derivatives_i)
+
+        return derivatives
+
+    def munge_data(self, model_pth, data_dir, cols, n_components,
+                   first_n=1000):
+
+        dfs = self._make_hmm_data(data_dir, cols, sort=True, sort_col="epoch",
+                                  first_n=first_n)
+        data = np.vstack(
+            [np.apply_along_axis(zscore, 0, df.to_numpy()) for df in dfs]
+        )
+        lengths = [len(df) for df in dfs]
+
+        with open(model_pth, 'rb') as f:
+            models = pickle.load(f)
+
+        model = models['best_models'][n_components-1]
+        print(model.score(data, lengths=lengths))
+        best_predictions = break_list_by_lengths(
+            model.predict(data, lengths=lengths), lengths)
+
+        return model, data, best_predictions, lengths
+
+    def characterize_transition_between_phases(self, model, data,
+                                               best_predictions, cols,
+                                               lengths, i, j):
+        '''
+        Compute the average derivative for each feature, sort features by
+        highest absolute value
+        '''
+        features = self.get_features_for_transition(model,
+                                                    data, best_predictions,
+                                                    lengths, i, j)
+
+        print(f"Number of times transition happened: {len(features)}")
+        features = np.mean(features, axis=0)
+        order = np.argsort(np.abs(features))[::-1]
+
+        feature_changes = np.array(self.get_difference_bt_means(model, i, j))
+
+        return cols[order], feature_changes[order]
+
+    def characterize_all_transitions(self, model, data, best_predictions, cols,
+                                     lengths):
+
+        phases = list(set(best_predictions))
+
+        transitions = {}
+
+        for i in phases:
+            for j in phases:
+                if i != j:
+                    cols, feature_changes = self.characterize_transition_between_phases(model, data, best_predictions, cols, lengths, i, j)
+                    transition_key = str(i) + '>>' + str(j)
+                    transitions[transition_key]['cols'] = cols
+                    transitions[transition_key]['feature_changes'] = feature_changes
+
+        return transitions
