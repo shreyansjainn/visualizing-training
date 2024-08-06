d482d4dacc81514bbf7529f37a4f74f586bf0ce8
shreyansjainn <jshrey8@gmail.com>
2024-08-06 16:15:19 +0530

    modified visualize module, misc changes in hmm module and color pallette

--- visualize_training/constants.py
+++ visualize_training/constants.py
@@ -1,3 +1,3 @@
 
-HEX_VALUES = ['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3',
-              '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3']
+HEX_VALUES = ['#be0aff', '#ff0000', '#a1ff0a', '#580aff',
+              '#ff8700', '#0aff99', '#deff0a', '#008bf8']
