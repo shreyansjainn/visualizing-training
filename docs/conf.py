c8eb0628bbe68d6e3949ea1c69cf7c532139071e
shreyansjainn <jshrey8@gmail.com>
2024-10-15 21:00:17 +0530

    updated conf.py for workflow

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,24 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
+from types import ModuleType
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath('../'))
+
+# Create a fake collections module
+collections = ModuleType('collections')
+sys.modules['collections'] = collections
+
+# Import the real collections
+import collections as real_collections
+
+# Add everything from the real collections to our fake one
+for attr in dir(real_collections):
+    setattr(collections, attr, getattr(real_collections, attr))
+
+# Add Callable from collections.abc
+from collections.abc import Callable
+collections.Callable = Callable
 
 project = 'Visualize Training'
 copyright = '2024, shreyans jain'
